d9cc5f1a3486955b24ce073534593bb60932f6c0
Proxeria <napinpdx@me.com>
2022-02-20 12:43:37 -0800

    stack implemented- not functional

--- shuntingYard.cpp
+++ shuntingYard.cpp
@@ -3,53 +3,9 @@
 #include<stdio>
 
 using namespace std;
-/*
-while there are tokens to be read:
-    read a token
-    if the token is:
-    - a number:
-        put it into the output queue
-    - a function:
-        push it onto the operator stack 
-    - an operator o1:
-        while (
-            there is an operator o2 other than the left parenthesis at the top
-            of the operator stack, and (o2 has greater precedence than o1
-            or they have the same precedence and o1 is left-associative)
-        ):
-            pop o2 from the operator stack into the output queue
-        push o1 onto the operator stack
-    - a left parenthesis (i.e. "("):
-        push it onto the operator stack
-    - a right parenthesis (i.e. ")"):
-        while the operator at the top of the operator stack is not a left parenthesis:
-            {assert the operator stack is not empty}
-            /* If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. */
-//    pop the operator from the operator stack into the output queue
-	    //    {assert there is a left parenthesis at the top of the operator stack}
-// pop the left parenthesis from the operator stack and discard it
-	//	if there is a function token at the top of the operator stack, then:
-	      //            pop the function from the operator stack into the output queue
-	      /* After the while loop, pop the remaining items from the operator stack into the output queue. */
-	      //  while there are tokens on the operator stack:
-	      /* If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses. */
-	      /*
-	{assert the operator on top of the stack is not a (left) parenthesis}
-    pop the operator from the operator stack onto the output queue
 
 
- */
-struct Node {
-  int data;
-  struct Node *next;
-};
-struct Node* top = NULL;
-void push(int val) {
-  struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
-  newnode->data = val;
-  newnode->next = top;
-  top = newnode;
-}
+
 void pop() {
   if(top==NULL)
     cout<<"Stack Underflow"<<endl;
@@ -58,52 +14,163 @@ void pop() {
     top = top->next;
   }
 }
-void display() {
-  struct Node* ptr;
-  if(top==NULL)
-    cout<<"stack is empty";
-  else {
-    ptr = top;
-    cout<<"Stack elements are: ";
-    while (ptr != NULL) {
-      cout<< ptr->data <<" ";
-      ptr = ptr->next;
-    }
-  }
-  cout<<endl;
-}
-int main() {
-  int ch, val;
-  cout<<"1) Push in stack"<<endl;
-  cout<<"2) Pop from stack"<<endl;
-  cout<<"3) Display stack"<<endl;
-  cout<<"4) Exit"<<endl;
-  do {
-    cout<<"Enter choice: "<<endl;
-    cin>>ch;
-    switch(ch) {
-    case 1: {
-      cout<<"Enter value to be pushed:"<<endl;
-      cin>>val;
-      push(val);
-      break;
-    }
-    case 2: {
-      pop();
-      break;
-    }
-    case 3: {
-      display();
-      break;
-    }
-    case 4: {
-      cout<<"Exit"<<endl;
-      break;
-    }
-    default: {
-      cout<<"Invalid Choice"<<endl;
-    }
-    }
-  }while(ch!=4);
+
+int precedence(char op){
+  if(op == '+'||op == '-')
+    return 1;
+  if(op == '*'||op == '/')
+    return 2;
   return 0;
+}
+
+// Function to perform arithmetic operations.
+int applyOp(int a, int b, char op){
+  switch(op){
+  case '+': return a + b;
+  case '-': return a - b;
+  case '*': return a * b;
+  case '/': return a / b;
+  }
+}
+
+// Function that returns value of
+// expression after evaluation.
+int evaluate(string tokens){
+  int i;
+
+  // stack to store integer values.
+  stack <int> values;
+
+  // stack to store operators.
+  stack <char> ops;
+
+  for(i = 0; i < tokens.length(); i++){
+
+    // Current token is a whitespace,
+    // skip it.
+    if(tokens[i] == ' ')
+      continue;
+
+    // Current token is an opening
+    // brace, push it to 'ops'
+    else if(tokens[i] == '('){
+      ops.push(tokens[i]);
+    }
+
+    // Current token is a number, push
+    // it to stack for numbers.
+    else if(isdigit(tokens[i])){
+      int val = 0;
+
+      // There may be more than one
+      // digits in number.
+      while(i < tokens.length() &&
+	    isdigit(tokens[i]))
+	{
+	  val = (val*10) + (tokens[i]-'0');
+	  i++;
+	}
+
+      values.push(val);
+
+      // right now the i points to
+      // the character next to the digit,
+      // since the for loop also increases
+      // the i, we would skip one
+      //  token position; we need to
+      // decrease the value of i by 1 to
+      // correct the offset.
+      i--;
+    }
+
+    // Closing brace encountered, solve
+    // entire brace.
+    else if(tokens[i] == ')')
+      {
+	while(!ops.empty() && ops.top() != '(')
+	  {
+	    int val2 = values.top();
+	    values.pop();
+
+	    int val1 = values.top();
+	    values.pop();
+
+	    char op = ops.top();
+	    ops.pop();
+
+	    values.push(applyOp(val1, val2, op));
+	  }
+
+	// pop opening brace.
+	if(!ops.empty())
+	  ops.pop();
+      }
+
+    // Current token is an operator.
+    else
+      {
+	// While top of 'ops' has same or greater
+	// precedence to current token, which
+	// is an operator. Apply operator on top
+	// of 'ops' to top two elements in values stack.
+	while(!ops.empty() && precedence(ops.top())
+	      >= precedence(tokens[i])){
+	  int val2 = values.top();
+	  values.pop();
+
+	  int val1 = values.top();
+	  values.pop();
+
+	  char op = ops.top();
+	  ops.pop();
+
+	  values.push(applyOp(val1, val2, op));
+	}
+
+	// Push current token to 'ops'.
+	ops.push(tokens[i]);
+      }
+  }
+
+  // Entire expression has been parsed at this
+  // point, apply remaining ops to remaining
+  // values.
+  while(!ops.empty()){
+    int val2 = values.top();
+    values.pop();
+
+    int val1 = values.top();
+    values.pop();
+
+    char op = ops.top();
+    ops.pop();
+
+    values.push(applyOp(val1, val2, op));
+  }
+
+  // Top of 'values' contains result, return it.
+  return values.top();
+}
+
+
+int main() {
+  cout << evaluate("10 + 2 * 6") << "\n";
+  cout << evaluate("100 * 2 + 12") << "\n";
+  cout << evaluate("100 * ( 2 + 12 )") << "\n";
+  cout << evaluate("100 * ( 2 + 12 ) / 14");
+  return 0;
+
+
+  
+  //split strings here vvv
+  string s = "Somewhere down the road";
+  istringstream iss(s);
+
+      do
+	{
+	  string subs;
+	  iss >> subs;
+	  cout << "Substring: " << subs << endl;
+	} while (iss);
+      
 }
