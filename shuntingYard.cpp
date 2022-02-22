303dfedc7678e6927e222ed723f44e95fa3b026c
Proxeria <napinpdx@me.com>
2022-02-21 16:09:57 -0800

    Tokenizer works

--- shuntingYard.cpp
+++ shuntingYard.cpp
@@ -1,197 +1,33 @@
 #include<iostream>
 #include<vector>
-#include<stdio>
+#include<sstream>
+#include<iterator>
+#include<string>
 
 using namespace std;
 
-
-
-void pop() {
-  if(top==NULL)
-    cout<<"Stack Underflow"<<endl;
-  else {
-    cout<<"The popped element is "<< top->data <<endl;
-    top = top->next;
+//https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+  std::istringstream iss(s);
+  std::string item;
+  while (std::getline(iss, item, delim)) {
+    *result++ = item;
   }
 }
 
-int precedence(char op){
-  if(op == '+'||op == '-')
-    return 1;
-  if(op == '*'||op == '/')
-    return 2;
-  return 0;
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, std::back_inserter(elems));
+  return elems;
 }
-
-// Function to perform arithmetic operations.
-int applyOp(int a, int b, char op){
-  switch(op){
-  case '+': return a + b;
-  case '-': return a - b;
-  case '*': return a * b;
-  case '/': return a / b;
-  }
-}
-
-// Function that returns value of
-// expression after evaluation.
-int evaluate(string tokens){
-  int i;
-
-  // stack to store integer values.
-  stack <int> values;
-
-  // stack to store operators.
-  stack <char> ops;
-
-  for(i = 0; i < tokens.length(); i++){
-
-    // Current token is a whitespace,
-    // skip it.
-    if(tokens[i] == ' ')
-      continue;
-
-    // Current token is an opening
-    // brace, push it to 'ops'
-    else if(tokens[i] == '('){
-      ops.push(tokens[i]);
-    }
-
-    // Current token is a number, push
-    // it to stack for numbers.
-    else if(isdigit(tokens[i])){
-      int val = 0;
-
-      // There may be more than one
-      // digits in number.
-      while(i < tokens.length() &&
-	    isdigit(tokens[i]))
-	{
-	  val = (val*10) + (tokens[i]-'0');
-	  i++;
-	}
-
-      values.push(val);
-
-      // right now the i points to
-      // the character next to the digit,
-      // since the for loop also increases
-      // the i, we would skip one
-      //  token position; we need to
-      // decrease the value of i by 1 to
-      // correct the offset.
-      i--;
-    }
-
-    // Closing brace encountered, solve
-    // entire brace.
-    else if(tokens[i] == ')')
-      {
-	while(!ops.empty() && ops.top() != '(')
-	  {
-	    int val2 = values.top();
-	    values.pop();
-
-	    int val1 = values.top();
-	    values.pop();
-
-	    char op = ops.top();
-	    ops.pop();
-
-	    values.push(applyOp(val1, val2, op));
-	  }
-
-	// pop opening brace.
-	if(!ops.empty())
-	  ops.pop();
-      }
-
-    // Current token is an operator.
-    else
-      {
-	// While top of 'ops' has same or greater
-	// precedence to current token, which
-	// is an operator. Apply operator on top
-	// of 'ops' to top two elements in values stack.
-	while(!ops.empty() && precedence(ops.top())
-	      >= precedence(tokens[i])){
-	  int val2 = values.top();
-	  values.pop();
-
-	  int val1 = values.top();
-	  values.pop();
-
-	  char op = ops.top();
-	  ops.pop();
-
-	  values.push(applyOp(val1, val2, op));
-	}
-
-	// Push current token to 'ops'.
-	ops.push(tokens[i]);
-      }
-  }
-
-  // Entire expression has been parsed at this
-  // point, apply remaining ops to remaining
-  // values.
-  while(!ops.empty()){
-    int val2 = values.top();
-    values.pop();
-
-    int val1 = values.top();
-    values.pop();
-
-    char op = ops.top();
-    ops.pop();
-
-    values.push(applyOp(val1, val2, op));
-  }
-
-  // Top of 'values' contains result, return it.
-  return values.top();
-}
-
 
 int main() {
-  cout << evaluate("10 + 2 * 6") << "\n";
-  cout << evaluate("100 * 2 + 12") << "\n";
-  cout << evaluate("100 * ( 2 + 12 )") << "\n";
-  cout << evaluate("100 * ( 2 + 12 ) / 14");
+  string testStr = "1 + 2";
+  std::vector<std::string> testVec = split(testStr, ' ');
+
+  for (string  thisToken: testVec) {
+    cout << thisToken << endl;
+  }
   return 0;
-
-
-  
-  //split strings here vvv
-  string s = "Somewhere down the road";
-  istringstream iss(s);
-
-      do
-	{
-	  string subs;
-	  iss >> subs;
-	  cout << "Substring: " << subs << endl;
-	} while (iss);
-
-      //tokenizes string into a vector
-      string line = "this is a test string";
-
-      // Vector of string to save tokens
-      vector <string> tokens;
-
-      // stringstream class check1
-      stringstream check1(line);
-
-      string intermediate;
-
-      // Tokenizing w.r.t. space ' '
-      while(getline(check1, intermediate, ' '))
-	{
-	  tokens.push_back(intermediate);
-	}
-
-      // Printing the token vector
-      for(int i = 0; i < tokens.size(); i++)
-	cout << tokens[i] << '\n';
-      
 }
